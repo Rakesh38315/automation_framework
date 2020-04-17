d6bedfedef0bec37bd2e2dde5d3e17d4c9633d45
Rakesh Tiwari <rakesh.tiwari@fluke.com>
2020-04-17 23:17:31 +0530

    assignment

---
+++ assignment/src/test/java/assignment/utils/FileUtil.java
@@ -0,0 +1,25 @@
+package assignment.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class FileUtil {
+	
+	private static Properties properties = new Properties();
+	private static InputStream inputStream;
+	
+	public static String readFromPropertyFile(String propertyFilePath, String propertyName) {
+		try {
+		    inputStream = new FileInputStream(propertyFilePath);
+			properties.load(inputStream);
+			return properties.getProperty(propertyName);
+		} 
+		catch (IOException io) {
+			io.printStackTrace();
+		}
+		return null;
+	}
+
+}
